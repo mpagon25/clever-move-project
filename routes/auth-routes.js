ae16914aef11805451881635bb12e788a1c4e9ba
Manuel Pagés Gonzales <m.pago92@gmail.com>
2021-04-25 21:19:04 +0200

    add login modal, set form for sign up page and add routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,16 @@
 const authRouter = require('express').Router();
 
+authRouter.post('/login',(req, res, next)=>{
+
+    
+});
+
+authRouter.get('/signup',(req,res,next)=>{
+    res.render('signup');
+});
+
+authRouter.post('/signup',(req,res,next)=>{
+
+});
 
 module.exports = authRouter;
