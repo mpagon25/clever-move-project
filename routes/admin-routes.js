de6e0368b914005ce05e2cabc73bc098ad742723
Manuel Pagés Gonzales <m.pago92@gmail.com>
2021-04-28 15:53:25 +0200

    add list of card with existing schedules

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,13 +1,32 @@
 const adminRoutes = require("express").Router();
 const Schedule = require("../models/Schedule.model");
+const User = require("../models/User.model");
 const { adminDashAuth } = require("./auth/admin-auth");
 
 adminRoutes.get("/admin-dashboard", adminDashAuth, (req, res, next) => {
     Schedule.find()
         .populate('user')
         .populate('addressTo')
-        .then((schedules)=>{           
-            res.render("auth/admin-dashboard", {user: req.session.userInfo, schedules});
+        .then((schedules)=>{
+            User.find()
+                .populate('address')
+                .then((users)=>{
+                    let popSchedules = [];
+                    schedules.forEach((schedule)=>{
+                        users.forEach((user)=>{
+                            if(schedule.user._id == user.id){
+                                                             
+                                
+                            }
+
+                        });
+                        
+                                   
+                    });                   
+                    
+                    res.render("auth/admin-dashboard", {user: req.session.userInfo, schedules});
+                });          
+            
         })
         .catch((err)=>{
             next(err);
