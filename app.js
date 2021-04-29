edb6f690f11b8c5aac42c58ba9bc00a8fbe311c8
Clever Tedeku <clevertedeku@gmail.com>
2021-04-29 12:48:25 +0200

    add middleware to schedule button on homepage

--- app.js
+++ app.js
@@ -49,11 +49,12 @@ app.use(session({
     })
 }));
 
-//set global variable to check if user is logged in
-// app.use((req, res, next)=>{
-// //to convert to boolean
-//  req.app.locals.isLoggedIn = !!req.session.userInfo
-// })
+// set global variable to check if user is logged in
+app.use((req, res, next)=>{
+//to convert to boolean using !!
+ req.app.locals.isLoggedIn = !!req.session.userInfo
+ next()
+})
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
